8d1b268ad88ea65137c1540fdd8c5b3185431ede
Jerson Moreno <jersonmr@gmail.com>
2024-02-14 10:17:11 -0400

    Add form fields to client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Filament\Forms\Components\LocalizedCountrySelect;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
@@ -23,10 +25,10 @@ class Client extends Model
         'email',
         'phone',
         'address',
+        'country',
         'city',
         'state',
         'zip',
-        'country',
     ];
 
     /**
@@ -39,23 +41,46 @@ class Client extends Model
         'user_id' => 'integer',
     ];
 
-    public function banks(): HasMany
-    {
-        return $this->hasMany(Bank::class);
-    }
-
-    public function invoices(): HasMany
-    {
-        return $this->hasMany(Invoice::class);
-    }
-
-    public function paymentGateways(): HasMany
-    {
-        return $this->hasMany(PaymentGateway::class);
-    }
-
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            TextInput::make('name')
+                ->label('Company Name')
+                ->required(),
+            TextInput::make('contact')
+                ->label('Contact Name')
+                ->required(),
+            TextInput::make('email')
+                ->label('Email')
+                ->email()
+                ->required(),
+            TextInput::make('phone')
+                ->label('Phone')
+                ->required()
+                ->tel(),
+            LocalizedCountrySelect::make('country')
+                ->label('Country')
+                ->required()
+                ->searchable(),
+            TextInput::make('address')
+                ->label('Address')
+                ->required()
+                ->columnSpanFull(),
+            Group::make()
+                ->columns(3)
+                ->schema([
+                             TextInput::make('city')
+                                 ->label('City'),
+                             TextInput::make('state')
+                                 ->label('State'),
+                             TextInput::make('zip')
+                                 ->label('Zip'),
+                         ]),
+        ];
     }
 }
